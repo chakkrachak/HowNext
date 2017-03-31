5937b5e72e8dc99419c4061ccc12291ed0176333
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-03-31 18:30:35 +0200

    Roadbook ok

--- HowNext/navitiasdk/src/main/java/org/kisio/labs/navitiasdk/access/NextDeparturesView.java
+++ HowNext/navitiasdk/src/main/java/org/kisio/labs/navitiasdk/access/NextDeparturesView.java
@@ -17,28 +17,35 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class NextDeparturesView {
     private ListView stopSchedulesListView;
     private ArrayAdapter adapter;
     private ArrayList<String> listItems;
     private AppCompatActivity linkedActivity;
+    private List<StopSchedule> currentStopSchedules;
 
     DateFormat navitiaDateFormat = new SimpleDateFormat("yyyyMMdd'T'HHmmss");
+
     public NextDeparturesView(AppCompatActivity linkedActivity, int stopSchedulesListViewId) {
         this.linkedActivity = linkedActivity;
-        stopSchedulesListView = (ListView) this.linkedActivity.findViewById(stopSchedulesListViewId);
-        listItems = new ArrayList<String>();
+        this.stopSchedulesListView = (ListView) this.linkedActivity.findViewById(stopSchedulesListViewId);
+        this.listItems = new ArrayList<String>();
+        this.currentStopSchedules = new ArrayList<StopSchedule>();
 
         new GetStopSchedulesAsyncTask(this.listItems).execute();
 
-        adapter = new ArrayAdapter(this.linkedActivity, android.R.layout.simple_list_item_1, listItems);
-        stopSchedulesListView.setAdapter(adapter);
+        this.adapter = new ArrayAdapter(this.linkedActivity, android.R.layout.simple_list_item_1, listItems);
+        this.stopSchedulesListView.setAdapter(adapter);
 
         stopSchedulesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent intent = new Intent(linkedActivity, RoadBookActivity.class);
+                intent.putExtra("from", "2.3880234;48.8378968");
+                String to = currentStopSchedules.get(i).getStopPoint().getCoord().getLon() + ";" + currentStopSchedules.get(i).getStopPoint().getCoord().getLat();
+                intent.putExtra("to", to);
                 linkedActivity.startActivity(intent);
             }
         });
@@ -57,7 +64,8 @@ public class NextDeparturesView {
                 DataFetcher<StopSchedulesResponse> dataFetcher = new DataFetcher<>(StopSchedulesResponse.class, "9e304161-bb97-4210-b13d-c71eaf58961c");
                 StopSchedulesResponse stopSchedulesResponse = dataFetcher.fetch("https://api.navitia.io/v1/coverage/fr-idf/coords/2.377310%3B48.847002/stop_schedules?distance=500&count=20&");
                 Date now = new Date();
-                for (StopSchedule stopSchedule : stopSchedulesResponse.getStopSchedules()) {
+                currentStopSchedules = stopSchedulesResponse.getStopSchedules();
+                for (StopSchedule stopSchedule : currentStopSchedules) {
                     Date currentDate = null;
                     try {
                         currentDate = navitiaDateFormat.parse(stopSchedule.getDateTimes().get(0).getDateTime());
--- HowNext/navitiasdk/src/main/java/org/kisio/labs/navitiasdk/access/RoadBookActivity.java
+++ HowNext/navitiasdk/src/main/java/org/kisio/labs/navitiasdk/access/RoadBookActivity.java
@@ -14,7 +14,6 @@ import org.kisio.labs.navitiasdk.models.journeys.Section;
 import org.kisio.labs.navitiasdk.utils.DataFetcher;
 
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -32,16 +31,20 @@ public class RoadBookActivity extends AppCompatActivity {
 
         roadBookItems = new ArrayList<String>();
 
-        new GetJourneyAsyncTask(this.roadBookItems).execute();
+        new GetJourneyAsyncTask(getIntent().getStringExtra("from"), getIntent().getStringExtra("to") , this.roadBookItems).execute();
 
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, roadBookItems);
         roadBookListView.setAdapter(adapter);
     }
 
     class GetJourneyAsyncTask extends AsyncTask<Void, Void, Void> {
+        private String from;
+        private String to;
         private ArrayList<String> listItems;
 
-        public GetJourneyAsyncTask(ArrayList<String> listItems) {
+        public GetJourneyAsyncTask(String from, String to, ArrayList<String> listItems) {
+            this.from = from;
+            this.to = to;
             this.listItems = listItems;
         }
 
@@ -49,7 +52,7 @@ public class RoadBookActivity extends AppCompatActivity {
         protected Void doInBackground(Void... voids) {
             try {
                 DataFetcher<JourneysResponse> dataFetcher = new DataFetcher<>(JourneysResponse.class, "9e304161-bb97-4210-b13d-c71eaf58961c");
-                JourneysResponse journeysResponse = dataFetcher.fetch("https://api.navitia.io/v1/coverage/fr-idf/journeys?to=2.373686;48.845796&from=2.3880234;48.8378968");
+                JourneysResponse journeysResponse = dataFetcher.fetch("https://api.navitia.io/v1/coverage/fr-idf/journeys?to="+this.to+"&from="+this.from);
                 Date now = new Date();
                 for (Journey journey : journeysResponse.getJourneys()) {
                     for (Section section : journey.getSections()) {
